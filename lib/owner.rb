5ce3834b6087cdbe2f57c8e1c5e7689f324dddbf
Malcolm Leake <echelon1089@gmail.com>
2019-03-28 01:50:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -73,7 +73,7 @@ class Owner
       
     
     def list_pets
-      "I have #{@pets[:fish].length} fish, #{@pets[:dogs].length} dog (s), #{@pets[:cats].length} cat (s)."
+      "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog (s), #{@pets[:cats].length} cat (s)."
       #binding.pry
     end
   
